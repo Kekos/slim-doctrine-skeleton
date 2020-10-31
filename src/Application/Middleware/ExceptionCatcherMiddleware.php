3b0048b70094af1f604fb110c2c0280e19eb0ca0
Christoffer Lindahl <christoffer@kekos.se>
2020-10-31 22:19:36 +0100

    Format assertion errors as JSON error descriptions.

--- src/Application/Middleware/ExceptionCatcherMiddleware.php
+++ src/Application/Middleware/ExceptionCatcherMiddleware.php
@@ -2,6 +2,9 @@
 
 namespace App\Application\Middleware;
 
+use Assert\AssertionFailedException;
+use Assert\InvalidArgumentException;
+use Assert\LazyAssertionException;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -9,6 +12,7 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Slim\Exception\HttpException;
 
+use function array_map;
 use function json_encode;
 
 use const JSON_THROW_ON_ERROR;
@@ -26,6 +30,31 @@ class ExceptionCatcherMiddleware implements MiddlewareInterface
     {
         try {
             $response = $handler->handle($request);
+
+        } catch (AssertionFailedException $ex) {
+            $response = $this->response_factory->createResponse(400)
+                ->withHeader('Content-Type', 'application/json');
+
+            $message = $ex->getMessage();
+            $details = [];
+
+            if ($ex instanceof LazyAssertionException) {
+                $message = 'Assertions failed';
+                $details = array_map(
+                    fn(InvalidArgumentException $ex) => $ex->getMessage(),
+                    $ex->getErrorExceptions(),
+                );
+            }
+
+            $response->getBody()->write(
+                json_encode(
+                    [
+                        'error' => $message,
+                        'details' => $details,
+                    ],
+                    JSON_THROW_ON_ERROR,
+                )
+            );
 
         } catch (HttpException $ex) {
             $response = $this->response_factory->createResponse($ex->getCode())
