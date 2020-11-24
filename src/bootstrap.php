d0385bee9612cb0ee8a9146695f0d9c9e2c3efce
Christoffer Lindahl <christoffer@kekos.se>
2020-11-24 16:13:13 +0100

    Load Dotenv in "unsafe" mode to make "getenv()" work as required by PHP-DI.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -12,7 +12,7 @@ if (empty($_SERVER['APP_ENV'])) {
         throw new RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "vlucas/phpdotenv" as a Composer dependency to load variables from a .env file.');
     }
 
-    $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv = Dotenv::createUnsafeImmutable(dirname(__DIR__));
     $dotenv->load();
 }
 
