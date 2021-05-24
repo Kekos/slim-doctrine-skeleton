26665d6b2d1372f6d72ba11475c73f1093b83053
Christoffer Lindahl <christoffer@kekos.se>
2021-05-24 18:43:13 +0200

    Common infrastructure, improved the message and trace in handler for PHP shutdown errors.

--- src/Common/Application/Handlers/ShutdownHandler.php
+++ src/Common/Application/Handlers/ShutdownHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Common\Application\Handlers;
 
+use ErrorException;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Exception\HttpInternalServerErrorException;
 use Slim\Interfaces\ErrorHandlerInterface;
@@ -28,9 +29,20 @@ class ShutdownHandler
             return;
         }
 
-        $message = 'An error while processing your request. Please try again later.';
+        $message = $error['message'];
 
-        $exception = new HttpInternalServerErrorException($this->request, $message);
+        $exception = new HttpInternalServerErrorException(
+            $this->request,
+            $message,
+            new ErrorException(
+                $message,
+                0,
+                $error['type'],
+                $error['file'],
+                $error['line'],
+            ),
+        );
+
         $response = $this->error_handler->__invoke(
             $this->request,
             $exception,
