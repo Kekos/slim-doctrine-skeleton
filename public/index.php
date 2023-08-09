5c89f2c2ee8bf603d897dec126d92013e51436d8
Christoffer Lindahl <christoffer@kekos.se>
2023-08-09 22:14:45 +0200

    Use correct order for production error middleware or else a 500 error is returned for 404 errors.

--- public/index.php
+++ public/index.php
@@ -29,22 +29,20 @@ $request = $server_request_creator->createServerRequestFromGlobals();
 $middleware = require __DIR__ . '/../config/middleware.php';
 $middleware($app);
 
-if (isset($error_handler)) {
-    $app->add($error_handler->getMiddleware());
-}
-
 // Register routes
 $routes = require __DIR__ . '/../config/routes.php';
 $routes($app);
+
+// Add Routing Middleware
+$app->addRoutingMiddleware();
 
 if (isset($error_handler)) {
     // Create Shutdown Handler
     $shutdown_handler = new ShutdownHandler($request, $error_handler->getErrorHandler());
     register_shutdown_function($shutdown_handler);
-}
 
-// Add Routing Middleware
-$app->addRoutingMiddleware();
+    $app->add($error_handler->getMiddleware());
+}
 
 // Run App & Emit Response
 $response = $app->handle($request);
